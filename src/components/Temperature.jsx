570746103ac37537f439187172dd210706545dd3
Animeshmitra08 <animeshmitra003@gmail.com>
2024-02-10 16:20:38 +0530

    made some changes in temperature(changes in timestamp and done some styling) and navbar

--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -2,38 +2,53 @@ import React from 'react'
 
 const Temperature = ({weatherData}) => {
   var weatherIcon = weatherData.weather[0].icon;
+  const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  let dateTime = new Date();
+  let day = weekday[dateTime.getDay()];
+  let time = dateTime.toLocaleTimeString('en-US');
+
+
+  let sunrise = weatherData.sys.sunrise;
+  let rise_militime = new Date(sunrise*1000);
+  let rise_time = rise_militime.toLocaleTimeString();
+  let sunset = weatherData.sys.sunset;
+  let set_militime = new Date(sunset*1000);
+  let set_time = set_militime.toLocaleTimeString();
   return (
     <>
         <div className="w-full bg-slate-300 h-60 rounded-xl p-4 shadow-xl flex gap-2 col-span-5 justify-evenly items-end">
-            <div className="flex flex-col gap-3">
-              <div className="flex">              
-                <div className="text-9xl">
-                  {weatherData.main.temp}
+            <div className="flex flex-col justify-center md:gap-0 gap-3">
+              <p className="font-semibold md:text-xl">Temperature:</p>
+              <div className="flex">       
+                <div className="md:text-9xl text-8xl">
+                  {weatherData.main.temp.toFixed()}
                 </div>              
                 <span>o</span>
-                <span className="text-4xl">C</span>
+                <span className="md:text-4xl text-2xl">C</span>
               </div>
-              <div className="text-4xl">
+              <div className="md:text-4xl text-2xl font-semibold">
                 <p>{weatherData.name}</p>
               </div>
             </div>
 
-            <div className="text-lg flex flex-col gap-2">
-              <p>Feels Like : {weatherData.main.feels_like}&deg;C</p>
+            <div className="flex flex-col gap-2 lg:text-xl">              
+              <p>Feels Like : {weatherData.main.feels_like.toFixed()}&deg;C</p>
               <p>Humidity : {weatherData.main.humidity}%</p>
               <p>Pressure : {weatherData.main.pressure}hPa</p>
             </div>
                       
-            <div className="text-lg">
+            <div className="lg:text-xl">
               <p>Latitude : {weatherData.coord.lat}</p>
               <p>Longitude : {weatherData.coord.lon}</p>
             </div>
-            <div className="text-lg">
-              <p>Sunrise : {weatherData.sys.sunrise}</p>
-              <p>Sunrise : {weatherData.sys.sunset}</p>
+            <div className="lg:text-xl">
+              <p>Sunrise : {rise_time}</p>
+              <p>Sunrise : {set_time}</p>
             </div>
-            <div className="flex flex-col items-center text-2xl">
-              <img className="w-32" src={`http://openweathermap.org/img/w/${weatherIcon}.png`} alt="weather_icon"/>
+            <div className="flex flex-col items-center lg:text-2xl">
+              <p className="font-medium">{day}</p> 
+              <p className="font-medium">{time}</p>
+              <img className="w-28" src={`http://openweathermap.org/img/w/${weatherIcon}.png`} alt="weather_icon"/>
               <p>{weatherData.weather[0].description}</p>
             </div>
         </div>
